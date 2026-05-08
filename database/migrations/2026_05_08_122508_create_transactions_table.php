87b25ca3ebd575a0833f53b189469ab147dd29fd
chitzawaung <chitzawaung777@gmail.com>
2026-05-09 00:51:44 +0630

    remove unnecessary migration file

--- database/migrations/2026_05_08_122508_create_transactions_table.php
+++ database/migrations/2026_05_08_122508_create_transactions_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
             $table->integer('quantity');
             $table->decimal('total_amount', 10, 2);
+            $table->timestamp('purchased_at')->nullable();
             $table->timestamps();
         });
     }
